48d7f01ac17d3b2dde4b98fe2be1e1da161f7e76
mnadirs <mnseyfelioglu@gmail.com>
2021-03-24 16:28:56 +0300

    updated main.py and added the draw method and finished the drawing

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ print(new_same)
 #check the logo names and if input is engrave then initiliaz the starting point and draw the logo
 for j in range(len(new_logo)):
     logo_name_l = new_logo[j][1]
+    print(new_logo[j][2][1])
     for k in range(len(new_engrave)):
         logo_name_e = new_engrave[k][1]
         if(logo_name_l == logo_name_e):
@@ -71,18 +72,67 @@ for j in range(len(new_logo)):
             start_y = int(new_engrave[k][3])
             print(start_x,start_y)
 
+
+
+def draw(start_x ,start_y):
+    for i in range(len(new_logo)):
+        for k in range(len(new_logo[i][2])):
+            if (new_logo[1][2][k] == "L"):
+                surface[2 * (start_x) - 2][2 * (start_y) - 3] = "_"
+                start_x = start_x
+                start_y = start_y - 1
+            elif (new_logo[1][2][k] == "L"):
+                surface[2 * (start_x) - 2][2 * (start_y) - 3] = "_"
+                start_x = start_x
+                start_y = start_y - 1
+            elif (new_logo[1][2][k] == "U"):
+                surface[2 * (start_x) - 3][2 * (start_y) - 2] = "|"
+                start_x = start_x - 1
+                start_y = start_y
+            elif (new_logo[1][2][k] == "R"):
+                surface[2 * (start_x) - 2][2 * (start_y) - 1] = "_"
+                start_x = start_x
+                start_y = start_y + 1
+            elif (new_logo[1][2][k] == "D"):
+                surface[2 * (start_x) - 1][2 * (start_y) - 2] = "|"
+                start_x = start_x + 1
+                start_y = start_y
+            else:
+                break
+        for row in surface:
+            print(''.join(row))
+
+
 # this could be helpfull
 #[(’logo’, 2, 4), (’logo2’, 3, 4), 3, ’CMPE326’]
 
-surface[1][0] = "|"
-surface[2][1] = "_"
+draw(start_x,start_y)
+
+# if(new_logo[1][2][0] == "L"):
+#     surface[2*(start_x)-2][2*(start_y)-3] = "_"
+#     start_x = start_x
+#     start_y = start_y-1
+# if(new_logo[1][2][1] == "L"):
+#     surface[2 * (start_x) - 2][2 * (start_y) - 3] = "_"
+#     start_x = start_x
+#     start_y = start_y - 1
+# if(new_logo[1][2][2] == "U"):
+#     surface[2*(start_x)-3][2*(start_y)-2] = "|"
+#     start_x = start_x -1
+#     start_y = start_y
+# if(new_logo[1][2][9] == "R"):
+#     surface[2 * (start_x) - 2][2 * (start_y) - 1] = "_"
+#     start_x = start_x
+#     start_y = start_y +1
+# if(new_logo[1][2][17] == "D"):
+#     surface[2 * (start_x) - 1][2 * (start_y) - 2] = "|"
+#     start_x = start_x +1
+#     start_y = start_y
+#print(start_x)
+#print(start_y)
+#surface[1][0] = "|"
+#surface[2][1] = "_"
 # for printing the given matrix
-for row in surface:
-    print(''.join(row))
-
-
-
-
 
 
 # while True:
